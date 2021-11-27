ec20ddb7cf1194c486683ff4825ee87ea646ecee
mikolajadamski <mikolaj.adamski47@gmail.com>
2021-11-27 01:42:35 +0100

    remove data, add getter and setter annotation

--- server/src/main/java/put/pl/identifier/NamedEntityRequest.java
+++ server/src/main/java/put/pl/identifier/NamedEntityRequest.java
@@ -1,10 +1,12 @@
 package put.pl.identifier;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 public class NamedEntityRequest {
 
